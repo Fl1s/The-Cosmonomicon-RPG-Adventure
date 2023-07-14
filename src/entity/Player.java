d69efe539ca664d8969d234ca6702a416ec74a78
Max Romanov <maksima7th@gmail.com>
2023-07-14 11:25:12 +0900

    Commit 2
    Добавил спрайты главного героя, + добавил смену кадров его ходьбы

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -13,26 +16,69 @@ public class Player extends Entity {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
+
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Up1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Up1.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Down1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Down2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Left2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/Player_Right2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void update() {
         if (keyH.upPressed) {
+            direction = "up";
             y -= speed;
-        } else if (keyH.downPressed) y += speed;
-        else if (keyH.leftPressed) x -= speed;
-        else if (keyH.rightPressed) x += speed;
+        } else if (keyH.downPressed) {
+            direction = "down";
+            y += speed;
+        } else if (keyH.leftPressed) {
+            direction = "left";
+            x -= speed;
+        } else if (keyH.rightPressed) {
+            direction = "right";
+            x += speed;
+        }
 
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
+//        g2.setColor(Color.white);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
