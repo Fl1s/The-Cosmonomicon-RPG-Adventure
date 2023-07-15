54320f9f38551066fa7faf61661d67991ab7db4e
Max Romanov <maksima7th@gmail.com>
2023-07-15 16:59:57 +0900

    Commit 7
    + перерисовал некоторые спрайты, + добавил коллизию

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -17,8 +17,15 @@ public class Player extends Entity {
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
-        screenX = gp.screenWidth / 2 - (gp.tileSize/2);
-        screenY = gp.screenHeight / 2 - (gp.tileSize/2);
+        screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
+        screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidArea.width = 32;
+        solidArea.height = 32;
+
         setDefaultValues();
         getPlayerImage();
     }
@@ -54,18 +61,34 @@ public class Player extends Entity {
         if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
             if (keyH.upPressed) {
                 direction = "up";
-                worldY -= speed;
             } else if (keyH.downPressed) {
                 direction = "down";
-                worldY += speed;
             } else if (keyH.leftPressed) {
                 direction = "left";
-                worldX -= speed;
             } else if (keyH.rightPressed) {
                 direction = "right";
-                worldX += speed;
             }
+            //Чекать коллизию тайла
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
 
+            //Если коллизии нет, игрок способен к движению
+            if (collisionOn == false) {
+                switch (direction) {
+                    case "up":
+                        worldY -= speed;
+                        break;
+                    case "down":
+                        worldY += speed;
+                        break;
+                    case "left":
+                        worldX -= speed;
+                        break;
+                    case "right":
+                        worldX += speed;
+                        break;
+                }
+            }
             spriteCounter++;
             if (spriteCounter > 15) {
                 if (spriteNum == 1) {
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -29,6 +29,7 @@ public class GamePanel extends JPanel implements Runnable {
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+   public CollisionChecker cChecker = new CollisionChecker(this);
     public Player player = new Player(this, keyH);
 
     public GamePanel() {
@@ -44,30 +45,7 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
-    //    @Override
-//    public void run() {
-//        double drawInterval = 1000000000 / FPS;
-//        double nextDrawTime = System.nanoTime() + drawInterval;
-//
-//        while (gameThread != null) {
-//
-//            update();
-//
-//            repaint();
-//            try {
-//                double remainingTime = nextDrawTime - System.nanoTime();
-//                remainingTime = remainingTime / 1000000;
-//
-//                if (remainingTime < 0) remainingTime = 0;
-//
-//                Thread.sleep((long) remainingTime);
-//
-//                nextDrawTime += drawInterval;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+
     public void run() {
         double drawInterval = 1000000000 / FPS;
         double delta = 0;
